bad493332d883e251c55ef689b537ab2e4ade59a
vinuth <vinuthsriarampath@outlook.com>
2025-01-18 11:48:41 +0530

    Feature : Add institute_dashboard.dart file for landing dashboard for User(Institute)

---
+++ lib/Institute/institute_dashboard.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class InstituteDashboard extends StatefulWidget {
+  const InstituteDashboard({super.key});
+
+  @override
+  State<InstituteDashboard> createState() => _InstituteDashboardState();
+}
+
+class _InstituteDashboardState extends State<InstituteDashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
