3340e737b1c696846f64b1ea85661f930b3d77dd
vinuth <vinuthsriarampath@outlook.com>
2025-01-18 20:01:27 +0530

    Feature : Add drawer as an external widget for institute dashboard

---
+++ lib/widgets/institute_drawer.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+
+class InstituteDrawer extends StatefulWidget {
+  const InstituteDrawer({super.key});
+
+  @override
+  State<InstituteDrawer> createState() => _InstituteDrawerState();
+}
+
+class _InstituteDrawerState extends State<InstituteDrawer> {
+
+  int _expandedIndex = -1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          ListTile(
+            title: const Text('Dashboard'),
+            onTap: () {
+              Navigator.pushNamed(context, '/institute/dashboard');
+            },
+          ),
+          ExpansionTile(
+            title: const Text('Course'),
+            initiallyExpanded: _expandedIndex == 0,
+            onExpansionChanged: (expanded) {
+              setState(() {
+                _expandedIndex = expanded ? 0 : -1;
+              });
+            },
+            children: [
+              ListTile(
+                title: const Text('Add Course'),
+                onTap: () {
+                  Navigator.pushNamed(context, '/institute/course/add-course');
+                },
+              ),
+              ListTile(
+                title: const Text('Update Course'),
+                onTap: () {
+                  Navigator.pushNamed(context, '/institute/course/update');
+                },
+              ),
+              ListTile(
+                title: const Text('Delete Course'),
+                onTap: () {
+                  Navigator.pushNamed(context, '/institute/course/delete');
+                },
+              ),
+              ListTile(
+                title: const Text('Search Course'),
+                onTap: () {
+                  Navigator.pushNamed(context, '/institute/course/search');
+                },
+              ),
+              ListTile(
+                title: const Text('Manage Course Students'),
+                onTap: () {
+                  Navigator.pushNamed(context, '/institute/course/manage');
+                },
+              ),
+            ],
+          ),
+          ExpansionTile(
+            title: const Text('Student'),
+            initiallyExpanded: _expandedIndex == 2,
+            onExpansionChanged: (expanded) {
+              setState(() {
+                _expandedIndex = expanded ? 1 : -1;
+              });
+            },
+            children: [
+              ListTile(
+                title: const Text('Add Student'),
+                onTap: () {
+                  Navigator.pushNamed(context, '/institute/student/add-student');
+                },
+              ),
+              ListTile(
+                title: const Text('Update Student'),
+                onTap: () {
+                  Navigator.pushNamed(context, '/institute/student/update');
+                },
+              ),
+              ListTile(
+                title: const Text('Remove Student'),
+                onTap: () {
+                  Navigator.pushNamed(context, '/institute/student/delete');
+                },
+              ),
+              ListTile(
+                title: const Text('Search Student'),
+                onTap: () {
+                  Navigator.pushNamed(context, '/institute/student/search');
+                },
+              ),
+            ],
+          ),
+          ExpansionTile(
+            title: const Text('Teacher'),
+            initiallyExpanded: _expandedIndex == 2,
+            onExpansionChanged: (expanded) {
+              setState(() {
+                _expandedIndex = expanded ? 2 : -1;
+              });
+            },
+            children: [
+              ListTile(
+                title: const Text('Add Teacher'),
+                onTap: () {
+                  Navigator.pushNamed(context, '/institute/teacher/add-teacher');
+                },
+              ),
+              ListTile(
+                title: const Text('Update Teacher'),
+                onTap: () {
+                  Navigator.pushNamed(context, '/institute/teacher/update');
+                },
+              ),
+              ListTile(
+                title: const Text('Remove Teacher'),
+                onTap: () {
+                  Navigator.pushNamed(context, '/institute/teacher/remove');
+                },
+              ),
+              ListTile(
+                title: const Text('Search Teacher'),
+                onTap: () {
+                  Navigator.pushNamed(context, '/institute/teacher/search');
+                },
+              ),
+            ],
+          ),
+          ListTile(
+            title: const Text('Profile'),
+            onTap: () {
+              Navigator.pushNamed(context, '/institute/profile');
+            },
+          ),
+          ListTile(
+            title: const Text('Sign out'),
+            onTap: () {
+              _logout(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+  void _logout(BuildContext context) {
+    Navigator.pushReplacementNamed(context, '/login');
+  }
+}
