ecf086f61f363ecdbc48a86bc96d53bc2bed774e
vinuth <vinuthsriarampath@outlook.com>
2025-02-08 07:13:30 +0530

    chore : setup provider scope

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tutiontoall_mobile/institute_register_otp.dart';
 
 import 'package:tutiontoall_mobile/login.dart';
 
 void main() => runApp(
-  DevicePreview(
-    enabled: !kReleaseMode,
-    builder: (context) => MyApp(),
+  ProviderScope(
+    child: DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => const MyApp(),
+    ),
   ),
 );
 class MyApp extends StatelessWidget {
@@ -17,7 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Login()
+      home: InstituteRegisterOtp()
     );
   }
 }
