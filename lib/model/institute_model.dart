b1e0eb3608d9dd0c854115ef6ccfe0fe3a680cd2
vinuth <vinuthsriarampath@outlook.com>
2025-02-12 08:22:29 +0530

    feature : add an institute_model.dart model

---
+++ lib/model/institute_model.dart
@@ -0,0 +1,17 @@
+class Institute{
+  String id="0";
+  final String name;
+  final String email;
+  final String contact;
+  final String address;
+  final String password;
+
+  Institute(
+      {
+        required this.name,
+        required this.email,
+        required this.contact,
+        required this.address,
+        required this.password
+      });
+}
