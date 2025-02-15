7ee27666bef00e57c9a49eb2c7c85aab640f1678
vinuth <vinuthsriarampath@outlook.com>
2025-02-15 08:14:36 +0530

    chore : load the .env file when the main method start executing

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,23 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:tutiontoall_mobile/institute_register_otp.dart';
 
 import 'package:tutiontoall_mobile/login.dart';
 
-void main() => runApp(
-  ProviderScope(
-    child: DevicePreview(
-      enabled: !kReleaseMode,
-      builder: (context) => const MyApp(),
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  runApp(
+    ProviderScope(
+      child: DevicePreview(
+        enabled: !kReleaseMode,
+        builder: (context) => const MyApp(),
+      ),
     ),
-  ),
-);
+  );
+}
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -21,7 +25,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: InstituteRegisterOtp()
+      home: Login()
     );
   }
 }
