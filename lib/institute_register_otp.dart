4fd37fcc192c126df3e9df643a13a60d12390282
vinuth <vinuthsriarampath@outlook.com>
2025-02-15 08:51:33 +0530

    feature : add a loading indicator until the response comes from the backend

--- lib/institute_register_otp.dart
+++ lib/institute_register_otp.dart
@@ -7,8 +7,10 @@ import 'package:pin_code_fields/pin_code_fields.dart';
 import 'package:http/http.dart' as http;
 
 import 'package:tutiontoall_mobile/login.dart';
+import 'package:tutiontoall_mobile/model/institute_model.dart';
 import 'package:tutiontoall_mobile/providers/OtpProvider.dart';
 import 'package:tutiontoall_mobile/providers/institute_provider.dart';
+import 'package:tutiontoall_mobile/providers/loading_provider.dart';
 import 'package:tutiontoall_mobile/widgets/alert.dart';
 
 class InstituteRegisterOtp extends ConsumerStatefulWidget {
@@ -22,46 +24,55 @@ class InstituteRegisterOtp extends ConsumerStatefulWidget {
 class _InstituteRegisterOtpState extends ConsumerState<InstituteRegisterOtp> {
   final TextEditingController otpController = TextEditingController();
 
-  @override
-  Widget build(BuildContext context) {
-    otpConfirmation(BuildContext context, WidgetRef ref) async {
-      String instituteBaseUrl = dotenv.env['INSTITUTE_BASEURL'] ?? '';
-      final otp = ref.watch(otpProvider);
-      final institute = ref.watch(instituteProvider);
-      String userOtp = otpController.text.trim();
-      if (userOtp.length < 6) {
-        showAlertDialog(context, "Invalid Otp", "otp is a 6 digit code");
-      } else {
-        if (otp == userOtp) {
-          var headers = {'Content-Type': 'application/json'};
-          var request = http.Request(
-              'POST', Uri.parse('$instituteBaseUrl/register'));
-          request.body = json.encode({
-            "name": institute.name,
-            "email": institute.email,
-            "contact": institute.contact,
-            "address": institute.address,
-            "password": institute.password
-          });
-          request.headers.addAll(headers);
+  otpConfirmation(BuildContext context, WidgetRef ref) async {
 
-          http.StreamedResponse response = await request.send();
+    ref.read(loadingProvider.notifier).state = true;
 
-          if (response.statusCode == 200) {
-            await showAlertDialog(context, "Registration Successful",
-                "You Have been successfully registered!");
-            Navigator.pushAndRemoveUntil(
-                context,
-                MaterialPageRoute(builder: (context) => const Login()),
-                (route) => false);
-          } else {
-            await showAlertDialog(context, "Registration Failed",
-                "Something went Wrong, Please try again!");
-            Navigator.pop(context);
-          }
+    String instituteBaseUrl = dotenv.env['INSTITUTE_BASEURL'] ?? '';
+
+    final otp = ref.watch(otpProvider);
+    final institute = ref.watch(instituteProvider);
+
+    String userOtp = otpController.text.trim();
+    if (userOtp.length < 6) {
+      showAlertDialog(context, "Invalid Otp", "otp is a 6 digit code");
+    } else {
+      if (otp == userOtp) {
+        var headers = {'Content-Type': 'application/json'};
+        var request =
+            http.Request('POST', Uri.parse('$instituteBaseUrl/register'));
+        request.body = json.encode({
+          "name": institute.name,
+          "email": institute.email,
+          "contact": institute.contact,
+          "address": institute.address,
+          "password": institute.password
+        });
+        request.headers.addAll(headers);
+
+        http.StreamedResponse response = await request.send();
+
+        if (response.statusCode == 200) {
+          ref.read(loadingProvider.notifier).state = false;
+          ref.read(instituteProvider.notifier).state = Institute(name: "", email: "", contact: "", address: "", password: "");
+          await showAlertDialog(context, "Registration Successful",
+              "You Have been successfully registered!");
+          Navigator.pushAndRemoveUntil(
+              context,
+              MaterialPageRoute(builder: (context) => const Login()),
+              (route) => false);
+        } else {
+          await showAlertDialog(context, "Registration Failed",
+              "Something went Wrong, Please try again!");
+          Navigator.pop(context);
         }
       }
     }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final isLoading = ref.watch(loadingProvider);
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -132,21 +143,26 @@ class _InstituteRegisterOtpState extends ConsumerState<InstituteRegisterOtp> {
                       height: 20,
                     ),
                     SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.8,
-                      child: ElevatedButton(
+                        width: MediaQuery.of(context).size.width * 0.8,
+                        child: ElevatedButton(
                           style: ElevatedButton.styleFrom(
                               backgroundColor: Colors.black,
                               foregroundColor: Colors.white,
                               padding: const EdgeInsets.symmetric(vertical: 15),
                               shape: RoundedRectangleBorder(
                                   borderRadius: BorderRadius.circular(10))),
-                          onPressed: () {
-                            otpConfirmation(context, ref);
-                          },
-                          child: const Text("Continue",
-                              style: TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.bold))),
-                    )
+                          onPressed: isLoading
+                              ? null
+                              : () => otpConfirmation(context, ref),
+                          child: isLoading
+                              ? const CircularProgressIndicator(
+                                  color: Colors.white,
+                                )
+                              : const Text("Continue",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold)),
+                        )),
                   ],
                 ),
               )
