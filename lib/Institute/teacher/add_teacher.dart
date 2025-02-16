4bbf67184521b77cad0ec331541a659981d03768
vinuth <vinuthsriarampath@outlook.com>
2025-02-16 13:08:27 +0530

    feature : implement add teacher function

--- lib/Institute/teacher/add_teacher.dart
+++ lib/Institute/teacher/add_teacher.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:tutiontoall_mobile/model/teacher_model.dart';
 import 'package:tutiontoall_mobile/providers/loading_provider.dart';
 import 'package:tutiontoall_mobile/widgets/alert.dart';
 import 'package:tutiontoall_mobile/widgets/institute_drawer.dart';
@@ -26,10 +28,15 @@ class _AddTeacherState extends ConsumerState<AddTeacher> {
   final TextEditingController passwordController = TextEditingController();
   final TextEditingController confirmPasswordController = TextEditingController();
 
+  final TextEditingController searchValueController = TextEditingController();
+
   bool isRegisteredTeacher = false;
+  String teacherBaseUrl = dotenv.env['TEACHER_BASEURL'] ?? '';
+  String instituteBaseUrl = dotenv.env['INSTITUTE_BASEURL'] ?? '';
+  Teacher? teacher;
+  bool isSearched = false;
 
   registerTeacher(BuildContext context, WidgetRef ref) async {
-
     ref.read(loadingProvider.notifier).state = true;
 
     String firstName = firstNameController.text.trim();
@@ -41,7 +48,6 @@ class _AddTeacherState extends ConsumerState<AddTeacher> {
     String password = passwordController.text.trim();
     String confirmPassword = confirmPasswordController.text.trim();
 
-    // Convert birthday from String to DateTime
     DateTime? birthday;
     if (birthdayText.isNotEmpty) {
       try {
@@ -55,12 +61,9 @@ class _AddTeacherState extends ConsumerState<AddTeacher> {
       return;
     }
 
-    String teacherBaseUrl = dotenv.env['TEACHER_BASEURL'] ?? '';
-
     if (validator(firstName, lastName, birthday, contactNumber, email, address,password, confirmPassword)) {
       var headers = {'Content-Type': 'application/json'};
-      var request = http.Request(
-          'POST', Uri.parse('$teacherBaseUrl/register'));
+      var request = http.Request('POST', Uri.parse('$teacherBaseUrl/register'));
       request.body = json.encode({
         "firstName": firstName,
         "lastName": lastName,
@@ -89,6 +92,80 @@ class _AddTeacherState extends ConsumerState<AddTeacher> {
         showAlertDialog(context, "Registration Failed", "$firstName $lastName Registration as a teacher failed");
         ref.read(loadingProvider.notifier).state = false;
       }
+    }
+  }
+
+  search() async{
+    ref.read(loadingProvider.notifier).state = true;
+    isSearched = true;
+    String searchValue = searchValueController.text.trim();
+
+    if (searchValue.isEmpty) {
+      showAlertDialog(context,'Username is Required', 'Please enter a username to search');
+      return;
+    }
+
+    if (searchValue.startsWith('T')) {
+      var request = http.Request('GET', Uri.parse('$teacherBaseUrl/search/$searchValue'));
+
+      http.StreamedResponse response = await request.send();
+
+      if (response.statusCode == 200) {
+        String responseString=await response.stream.bytesToString();
+        Map<String,dynamic> jsonResponse=jsonDecode(responseString);
+        teacher=Teacher.fromJson(jsonResponse);
+        ref.read(loadingProvider.notifier).state=false;
+      }
+      else {
+        ref.read(loadingProvider.notifier).state=false;
+        showAlertDialog(context,'Not Found', 'Searched Username Not found');
+        teacher=null;
+        isSearched=false;
+        return;
+      }
+    } else {
+      ref.read(loadingProvider.notifier).state=false;
+      showAlertDialog(context,'Invalid Username', 'Enter an Valid Teacher ID starts with T');
+      isSearched=false;
+      return;
+    }
+  }
+
+  addTeacherToInstitute(BuildContext context,WidgetRef ref) async {
+
+    ref.read(loadingProvider.notifier).state=true;
+
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? instituteId = prefs.getString('id');
+    print(isSearched);
+    if(isSearched){
+      var headers = {
+        'Content-Type': 'application/json'
+      };
+      var request = http.Request('POST', Uri.parse('$instituteBaseUrl/teachers/add'));
+      request.body = json.encode({
+        "teacherId": teacher!.id,
+        "instituteId": instituteId,
+        "date": DateTime.now().toIso8601String()
+      });
+      request.headers.addAll(headers);
+
+      http.StreamedResponse response = await request.send();
+
+      if (response.statusCode == 200) {
+        isSearched=false;
+        teacher=null;
+        ref.read(loadingProvider.notifier).state=false;
+        showAlertDialog(context, "Added Successfully", 'Teacher added to the institute successfully');
+      }
+      else {
+        ref.read(loadingProvider.notifier).state=false;
+        showAlertDialog(context, "Teacher Not Searched", "Search a teacher before adding to the institute");
+      }
+
+    }else{
+      ref.read(loadingProvider.notifier).state=false;
+      showAlertDialog(context, "Error occurred", "Some thing went wrong");
     }
   }
 
@@ -169,7 +246,140 @@ class _AddTeacherState extends ConsumerState<AddTeacher> {
                 height: 10,
               ),
               isRegisteredTeacher
-                  ? const Text("Search")
+                  ? Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 30),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.black26,
+                            blurRadius: 10,
+                            offset: Offset(0, 5),
+                          ),
+                        ],
+                      ),
+                      width: MediaQuery.of(context).size.width * 0.9,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Expanded(
+                                child: TextField(
+                                  controller: searchValueController,
+                                  decoration: InputDecoration(
+                                    hintText: "Search Teacher",
+                                    prefixIcon: const Icon(Icons.search,
+                                        color: Colors.grey),
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(width: 10),
+                              ElevatedButton(
+                                onPressed: isLoading
+                                    ? null
+                                    : () => search(),
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.black,
+                                  foregroundColor: Colors.white,
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 20, vertical: 15),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(30),
+                                  ),
+                                  elevation: 5,
+                                ),
+                                child: isLoading
+                                    ? const CircularProgressIndicator(
+                                        color: Colors.white)
+                                    : const Text(
+                                        "Search",
+                                        style: TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 16),
+                          const Text("Teacher Details", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                          const SizedBox(height: 16),
+                          if (teacher != null) ...[
+                            buildLabel("First Name", teacher!.firstName),
+                            buildLabel("Last Name", teacher!.lastName),
+                            buildLabel("Contact", teacher!.contact),
+                            buildLabel("Email", teacher!.email),
+                            buildLabel("Date of Birth",teacher!.dob.toLocal().toString().split(' ')[0]),
+                            buildLabel("Address", teacher!.address),
+                          ]else ...[
+                            buildLabel("First Name", ""),
+                            buildLabel("Last Name", ""),
+                            buildLabel("Contact", ""),
+                            buildLabel("Email", ""),
+                            buildLabel("Date of Birth",""),
+                            buildLabel("Address", ""),
+                          ],
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          isSearched ?
+                          SizedBox(
+                            width: double.infinity,
+                            child: ElevatedButton(
+                              onPressed: isLoading
+                                  ? null
+                                  : () =>addTeacherToInstitute(context,ref),
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.black,
+                                foregroundColor: Colors.white,
+                                padding:
+                                const EdgeInsets.symmetric(vertical: 15),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10)),
+                              ),
+                              child: isLoading
+                                  ? const CircularProgressIndicator(
+                                color: Colors.white,
+                              )
+                                  : const Text("Add To Institute",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold)),
+                            ),
+                          )
+                              :
+                          SizedBox(
+                            width: double.infinity,
+                            child: ElevatedButton(
+                              onPressed: (){
+                                showAlertDialog(context, "Teacher not searched", "Search a Teacher First");
+                              },
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.grey,
+                                foregroundColor: Colors.white,
+                                padding: const EdgeInsets.symmetric(vertical: 15),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                              child:const Text(
+                                "Search a Teacher",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
                   : Container(
                       padding: const EdgeInsets.symmetric(
                           horizontal: 20, vertical: 30),
@@ -249,11 +459,11 @@ class _AddTeacherState extends ConsumerState<AddTeacher> {
                               decoration: const InputDecoration(
                                   labelText: "Address",
                                   border: OutlineInputBorder())),
-                          const SizedBox(height: 20),
+                          const SizedBox(height: 15),
                           const Text("Login Credentials",
                               style: TextStyle(
                                   fontWeight: FontWeight.bold, fontSize: 20)),
-                          const SizedBox(height: 20),
+                          const SizedBox(height: 15),
                           TextField(
                               controller: passwordController,
                               obscureText: true,
@@ -267,22 +477,32 @@ class _AddTeacherState extends ConsumerState<AddTeacher> {
                               decoration: const InputDecoration(
                                   labelText: "Confirm Password",
                                   border: OutlineInputBorder())),
-                          const SizedBox(height: 25),
+                          const SizedBox(height: 15),
                           SizedBox(
                             width: double.infinity,
                             child: ElevatedButton(
-                              onPressed: isLoading ? null : () => registerTeacher(context, ref),
+                              onPressed: isLoading
+                                  ? null
+                                  : () => registerTeacher(context, ref),
                               style: ElevatedButton.styleFrom(
                                 backgroundColor: Colors.black,
                                 foregroundColor: Colors.white,
-                                padding: const EdgeInsets.symmetric(vertical: 15),
-                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 15),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10)),
                               ),
                               child: isLoading
-                                  ? const CircularProgressIndicator(color: Colors.white,)
-                                  : const Text("Register", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                                  ? const CircularProgressIndicator(
+                                      color: Colors.white,
+                                    )
+                                  : const Text("Register",
+                                      style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold)),
                             ),
                           ),
+
                         ],
                       ),
                     ),
@@ -292,4 +512,47 @@ class _AddTeacherState extends ConsumerState<AddTeacher> {
       ),
     );
   }
+
+  Widget buildLabel(String label, String value) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 6.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Icon for visual appeal (optional)
+          const Icon(
+            Icons.info_outline,
+            size: 18,
+            color: Colors.blueAccent,
+          ),
+          const SizedBox(width: 8),
+          // Label and value
+          Expanded(
+            child: RichText(
+              text: TextSpan(
+                children: [
+                  TextSpan(
+                    text: "$label : ",
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                      color: Colors.black87,
+                    ),
+                  ),
+                  TextSpan(
+                    text: value,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      color: Colors.black54,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
 }
