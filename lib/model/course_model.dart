7c86714dc23ff233a861563e153ed9824740d381
vinuth <vinuthsriarampath@outlook.com>
2025-02-21 06:41:32 +0530

    feature : add studentCourseList attribute

--- lib/model/course_model.dart
+++ lib/model/course_model.dart
@@ -1,12 +1,15 @@
 
+import 'package:tutiontoall_mobile/model/student_registered_courses.dart';
+
 class Course{
   final String id;
   final String name;
   final String type;
-  final String teacherId;
-  final String teacherName;
+  final String? teacherId;
+  final String? teacherName;
+  final List<StudentRegisteredCourses> studentCoursesList;
 
-  Course({required this.id, required this.name, required this.type, required this.teacherId, required this.teacherName});
+  Course({required this.id, required this.name, required this.type, required this.teacherId, required this.teacherName, required this.studentCoursesList});
 
   factory Course.fromJson(Map<String,dynamic> json){
     return Course(
@@ -14,7 +17,10 @@ class Course{
       name: json['name'],
       type: json['type'],
       teacherId: json['teacherId'],
-      teacherName: json['teacherName']
+      teacherName: json['teacherName'],
+      studentCoursesList: (json['studentCoursesList'] as List<dynamic>)
+            .map((e)=> StudentRegisteredCourses.fromJson(e))
+            .toList()
     );
   }
 }
